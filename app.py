775da620dfc1a95eb6e1230a36fea16426cbc0e4
mwakaambrose <ambrosemwaka@gmail.com>
2019-11-07 11:14:25 +0300

    gunicorn and wsgi

--- app.py
+++ app.py
@@ -255,4 +255,6 @@ def quick_sort(nums):
     return nums
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
